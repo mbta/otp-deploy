62db1a7176188f894b47cc0b74aa6a6113d03963
Paul Swartz <paulswartz@gmail.com>
2017-06-06 11:18:12 -0400

    use version of OTP from master

--- make_deploy.sh
+++ make_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm mbta_otp.zip
-zip mbta_otp.zip Procfile otp-1.1.0-shaded.jar var/graphs/mbta/Graph.obj var/graphs/mbta/*.json
+zip mbta_otp.zip Procfile otp-1.2.0-SNAPSHOT-shaded.jar var/graphs/mbta/Graph.obj var/graphs/mbta/*.json
