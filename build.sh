45836d86d1138f8dc68026cd9f95247ab60c651e
Paul Swartz <paul@paulswartz.net>
2018-07-09 07:46:53 -0400

    ensure we use Java 8 for building

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# ensure we use Java 8; other versions have an issue building the graph:
+# https://groups.google.com/forum/#!topic/opentripplanner-users/pvtm3BSyS9g
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
 java -Xmx4G -jar otp-1.2.0-shaded.jar --build var/graphs/mbta/ --basePath var/
