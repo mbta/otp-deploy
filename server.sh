e402990137a5825f66fde54f240e174d57a35748
Paul Swartz <paul@paulswartz.net>
2018-10-31 14:16:30 -0400

    update to OTP 1.3.0

--- server.sh
+++ server.sh
@@ -3,4 +3,4 @@ IN_MEMORY="--inMemory"
 if [ -f "var/graphs/mbta/Graph.obj" ]; then
     IN_MEMORY=""
 fi
-java -Xmx4G -jar otp-1.2.0-shaded.jar --server --basePath var/ $IN_MEMORY
+java -Xmx4G -jar otp-1.3.0-shaded.jar --server --basePath var/ $IN_MEMORY
