1271bb9c66ee1bc515b4610088d6e77a0580af54
lboyarsky <lboyarsky@mbta.com>
2019-02-08 11:45:51 -0500

    Update CI build script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # ensure we use Java 8; other versions have an issue building the graph:
 # https://groups.google.com/forum/#!topic/opentripplanner-users/pvtm3BSyS9g
 source /opt/change-java-version.sh
 change-java-version 8
 rm -rf OpenTripPlanner
-git clone git@github.com:mbta/OpenTripPlanner.git --depth 1
-pushd ${BUILD_DIR}/OpenTripPlanner
+git clone git@github.com:mbta/OpenTripPlanner.git
+pushd OpenTripPlanner
 mvn clean install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
-cp ${BUILD_DIR}/OpenTripPlanner/target/otp-1.4.0-SNAPSHOT-shaded.jar ${BUILD_DIR}
 popd
+cp ./OpenTripPlanner/target/otp-1.4.0-SNAPSHOT-shaded.jar .
 java -Xmx4G -jar otp-1.4.0-SNAPSHOT-shaded.jar --build var/graphs/mbta/ --basePath var/
