4f3ad4e48e31868a26f814c990bdffa264a0a307
lboyarsky <lboyarsky@mbta.com>
2019-02-08 11:14:56 -0500

    Enabled enhanced feed and switching to forked OTP

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 # ensure we use Java 8; other versions have an issue building the graph:
 # https://groups.google.com/forum/#!topic/opentripplanner-users/pvtm3BSyS9g
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-
-java -Xmx4G -jar otp-1.3.0-shaded.jar --build var/graphs/mbta/ --basePath var/
+rm -rf OpenTripPlanner
+git clone git@github.com:mbta/OpenTripPlanner.git --depth 1
+pushd OpenTripPlanner
+mvn clean install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
+popd
+cp ./OpenTripPlanner/target/otp-1.4.0-SNAPSHOT-shaded.jar .
+java -Xmx4G -jar otp-1.4.0-SNAPSHOT-shaded.jar --build var/graphs/mbta/ --basePath var/
