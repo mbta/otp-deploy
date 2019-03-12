765d972c342564c0d99db0f1814f5811b87b3b83
lboyarsky <lboyarsky@mbta.com>
2019-03-12 13:13:45 -0400

    Switching back to dev GTFS feed

--- update_gtfs.sh
+++ update_gtfs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -N https://mbta-gtfs-s3.s3.amazonaws.com/google_transit.zip -O var/graphs/mbta/google_transit.zip
+wget -N https://cdn.mbta.com/MBTA_GTFS.zip -O var/graphs/mbta/MBTA_GTFS.zip
 wget -N http://data.trilliumtransit.com/gtfs/loganexpress-ma-us/loganexpress-ma-us.zip -O var/graphs/mbta/loganexpress-ma-us.zip
