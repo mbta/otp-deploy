5f169590809d28a0fb4c724e57c5e9820ea2a905
lboyarsky <45011335+lboyarsky@users.noreply.github.com>
2019-03-12 13:18:46 -0400

    Switching back to dev GTFS feed (#9)

--- semaphore/gtfs_feed_version.py
+++ semaphore/gtfs_feed_version.py
@@ -2,7 +2,7 @@
 import csv
 import zipfile
 
-with zipfile.ZipFile("var/graphs/mbta/google_transit.zip", "r") as zf:
+with zipfile.ZipFile("var/graphs/mbta/MBTA_GTFS.zip", "r") as zf:
     with zf.open("feed_info.txt") as feed_info:
         for row in csv.DictReader(feed_info):
             print row["feed_version"]
