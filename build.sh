6ba12b97ab2ef1577e6c2a5044a9256c9dcd7bed
lboyarsky <45011335+lboyarsky@users.noreply.github.com>
2019-04-02 17:28:04 -0400

    Added step to build our OneBusAway gtfs library fork (#10)
    * Added stop to build our OneBusAway gtfs library fork
    
    * Switched to master branch

--- build.sh
+++ build.sh
@@ -4,10 +4,18 @@
 # https://groups.google.com/forum/#!topic/opentripplanner-users/pvtm3BSyS9g
 source /opt/change-java-version.sh
 change-java-version 8
+
+rm -rf onebusaway-gtfs-modules
+git clone git@github.com:mbta/onebusaway-gtfs-modules.git --depth 1
+pushd onebusaway-gtfs-modules
+mvn clean install -Dmaven.test.skip=true -Dlicense.skip=true
+popd
+
 rm -rf OpenTripPlanner
 git clone git@github.com:mbta/OpenTripPlanner.git --depth 1
 pushd OpenTripPlanner
 mvn clean install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
 popd
+
 cp ./OpenTripPlanner/target/otp-1.4.0-SNAPSHOT-shaded.jar .
 java -Xmx4G -jar otp-1.4.0-SNAPSHOT-shaded.jar --build var/graphs/mbta/ --basePath var/
