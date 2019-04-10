71b7a6481c8eeffc9a63c4a61ddff24dc6fe736a
lboyarsky <45011335+lboyarsky@users.noreply.github.com>
2019-04-10 15:44:22 -0400

    Sort input GTFS files while building (#12)
    * Added pre-defined order of GTFS files
    
    * Switched to custom branch
    
    * Switched back to master branch
    
    * Added comment explaining importance of file order

--- semaphore/gtfs_feed_version.py
+++ semaphore/gtfs_feed_version.py
@@ -2,7 +2,7 @@
 import csv
 import zipfile
 
-with zipfile.ZipFile("var/graphs/mbta/MBTA_GTFS.zip", "r") as zf:
+with zipfile.ZipFile("var/graphs/mbta/1_MBTA_GTFS.zip", "r") as zf:
     with zf.open("feed_info.txt") as feed_info:
         for row in csv.DictReader(feed_info):
             print row["feed_version"]
--- update_gtfs.sh
+++ update_gtfs.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-wget -N https://cdn.mbta.com/MBTA_GTFS.zip -O var/graphs/mbta/MBTA_GTFS.zip
-wget -N http://data.trilliumtransit.com/gtfs/loganexpress-ma-us/loganexpress-ma-us.zip -O var/graphs/mbta/loganexpress-ma-us.zip
+
+# it's important for MBTA_GFTS to be first file in the folder,
+# otherwise realtime alerts won't work: https://github.com/mbta/OpenTripPlanner/pull/8
+
+wget -N https://cdn.mbta.com/MBTA_GTFS.zip -O var/graphs/mbta/1_MBTA_GTFS.zip
+wget -N http://data.trilliumtransit.com/gtfs/loganexpress-ma-us/loganexpress-ma-us.zip -O var/graphs/mbta/2_loganexpress-ma-us.zip
