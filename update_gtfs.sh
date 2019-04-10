a2c07dddad111efbef672e7f7332ae48f10ad315
lboyarsky <lboyarsky@mbta.com>
2019-04-10 13:17:19 -0400

    Added pre-defined order of GTFS files

--- update_gtfs.sh
+++ update_gtfs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -N https://cdn.mbta.com/MBTA_GTFS.zip -O var/graphs/mbta/MBTA_GTFS.zip
-wget -N http://data.trilliumtransit.com/gtfs/loganexpress-ma-us/loganexpress-ma-us.zip -O var/graphs/mbta/loganexpress-ma-us.zip
+wget -N https://cdn.mbta.com/MBTA_GTFS.zip -O var/graphs/mbta/1_MBTA_GTFS.zip
+wget -N http://data.trilliumtransit.com/gtfs/loganexpress-ma-us/loganexpress-ma-us.zip -O var/graphs/mbta/2_loganexpress-ma-us.zip
