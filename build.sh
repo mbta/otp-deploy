1b802c6dab29ac32da7b797c478b3b7a25f34bcb
lboyarsky <lboyarsky@mbta.com>
2019-04-29 16:05:10 -0400

    Temporary switching to dev branches to run semaphore deploy to dev-green

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ source /opt/change-java-version.sh
 change-java-version 8
 
 rm -rf onebusaway-gtfs-modules
-git clone git@github.com:mbta/onebusaway-gtfs-modules.git --depth 1
+git clone git@github.com:mbta/onebusaway-gtfs-modules.git --depth 1 -b lev-support-for-pathways
 pushd onebusaway-gtfs-modules
 mvn clean install -Dmaven.test.skip=true -Dlicense.skip=true
 popd
 
 rm -rf OpenTripPlanner
-git clone git@github.com:mbta/OpenTripPlanner.git --depth 1
+git clone git@github.com:mbta/OpenTripPlanner.git --depth 1 -b lev-new-pathways
 pushd OpenTripPlanner
 mvn clean install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
 popd
