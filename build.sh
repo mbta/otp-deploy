1cd0deea48c7e96e2d1ef464b69d4ea868d515c8
lboyarsky <lboyarsky@mbta.com>
2019-07-09 11:17:40 -0400

    Addressing PR feedback

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ change-java-version 8
 rm -rf onebusaway-gtfs-modules
 git clone git@github.com:mbta/onebusaway-gtfs-modules.git --depth 1
 pushd onebusaway-gtfs-modules
-mvn clean install -Dmaven.test.skip=true -Dlicense.skip=true
+mvn install -Dmaven.test.skip=true -Dlicense.skip=true
 popd
 
 rm -rf OpenTripPlanner
 git clone git@github.com:mbta/OpenTripPlanner.git --depth 1
 pushd OpenTripPlanner
-mvn clean install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
+mvn install -Dmaven.test.skip=true -Dgpg.skip -Dmaven.javadoc.skip=true
 popd
 
 cp ./OpenTripPlanner/target/otp-1.4.0-SNAPSHOT-shaded.jar .
--- tests.sh
+++ tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+rm -rf OpenTripPlanner
+git clone git@github.com:mbta/OpenTripPlanner.git --depth 1
 pushd OpenTripPlanner
 mvn test -Dgpg.skip -Dmaven.javadoc.skip=true
 popd
