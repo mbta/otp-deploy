cb8fb0b83a6b63ec8df1e8be3e3c51059a529b37
lboyarsky <lboyarsky@mbta.com>
2019-08-29 12:01:36 -0400

    Ignoring error if OSM files don't exist

--- update_pbf.sh
+++ update_pbf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-rm var/graphs/mbta/*.pbf *.osm.pbf
+rm -rf var/graphs/mbta/*.pbf
 for filename in massachusetts-latest.osm.pbf rhode-island-latest.osm.pbf; do
     wget -nc -P var/graphs/mbta http://download.geofabrik.de/north-america/us/$filename
 done
