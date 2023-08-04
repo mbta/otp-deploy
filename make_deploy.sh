e94b634cfac93abef6dfd7246ef5284ff48d1817
Emma Simon <esimon@mbta.com>
2023-08-04 16:47:54 -0400

    Remove outdated files

--- make_deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rm mbta_otp.zip
-zip mbta_otp.zip Procfile otp-1.4.0*-shaded.jar var/graphs/mbta/Graph.obj var/graphs/mbta/*.json
--- semaphore/integration_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-export PIPENV_VERBOSITY=-1
-export PIPENV_CACHE_DIR=$SEMAPHORE_CACHE_DIR
-
-/usr/bin/env python3 -m pip install -U pip pipenv
-/usr/bin/env python3 -m pipenv sync -d
-/usr/bin/env python3 -m pipenv run python -m semaphore.integration_tests
